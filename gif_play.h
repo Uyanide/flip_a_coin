8a44eda4b68d50447a4d58ed819b0eb670b2d7ee
Peng <PYWang0608@foxmail.com>
2024-06-16 01:03:29 +0200

    优化可读性

--- gif_play.h
+++ gif_play.h
@@ -5,54 +5,56 @@
 #include <gdiplus.h>
 #pragma comment(lib, "gdiplus.lib")
 
-#define IDI_TIMER 1
-
 LRESULT CALLBACK gif_proc(HWND, UINT, WPARAM, LPARAM);
 
 class GIF_PLAYER
 {
 public:
-	GIF_PLAYER(HWND, const wchar_t*, COLORREF, UINT = 0, UINT = 0, bool = false);
-	GIF_PLAYER(HWND, LPWSTR, COLORREF, UINT = 0, UINT = 0, bool = false);
-	~GIF_PLAYER();
+    // Constructor
+    GIF_PLAYER(HWND, const wchar_t *, COLORREF, UINT = 0, UINT = 0, bool = false);
+    GIF_PLAYER(HWND, LPWSTR, COLORREF, UINT = 0, UINT = 0, bool = false);
+
+    // Destructor
+    ~GIF_PLAYER();
 
 private:
-	void create_subwindow(HWND);
+    // Private methods
+    void create_subwindow(HWND);
 
 public:
-	UINT get_frame_count();
-	UINT get_frame_delay();
-	Gdiplus::Rect get_gif_rect() const;
-
-	void draw_frame(HDC, UINT);
-	void draw_frame(HDC, UINT, bool DOUBLE_BUFFER);
-	void start();
-	void cease();
-	void pause();
+    // Public methods
+    UINT get_frame_count();
+    UINT get_frame_delay();
+    Gdiplus::Rect get_gif_rect() const;
+    void draw_frame(HDC, UINT);
+    void draw_frame(HDC, UINT, bool DOUBLE_BUFFER);
+    void start();
+    void cease();
+    void pause();
 
 private:
-	// information about the GIF resource
-	Gdiplus::Image* gif_src;
-	INT frame_count = -1;            // Number of frames in the GIF
-	INT frame_delay = -1;            // Frame delay in milliseconds
-	Gdiplus::Rect gif_rect;          // position and size of the GIF
-	RECT gif_RECT;                   // converted to RECT
-	Gdiplus::Color background_color; // color similar to the main color of the GIF
+    // Information about the GIF resource
+    Gdiplus::Image *gif_src;
+    INT frame_count = -1;            // Number of frames in the GIF
+    INT frame_delay = -1;            // Frame delay in milliseconds
+    Gdiplus::Rect gif_rect;          // Position and size of the GIF
+    RECT gif_RECT;                   // Converted to RECT
+    Gdiplus::Color background_color; // Color similar to the main color of the GIF
 
-	// case load from rc
-	IStream* pStream = NULL;
+    // Case load from rc
+    IStream *pStream = NULL;
 
-	// subwindow for the GIF
-	HWND gif_hwnd;
+    // Subwindow for the GIF
+    HWND gif_hwnd;
 
 public:
-	// GIF playback control
-	enum
-	{
-		PLAY,
-		PAUSE,
-		CEASE
-	} gif_state = CEASE;
-	INT current_frame = -1;
-	bool is_loop = false;
+    // GIF playback control
+    enum
+    {
+        PLAY,
+        PAUSE,
+        CEASE
+    } gif_state = CEASE;
+    INT current_frame = -1;
+    bool is_loop = false;
 };
--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@
 #include "resource.h"
 #include "gif_play.h"
 
-//const wchar_t* gif_path[] = { L"rsc\\c1.gif", L"rsc\\c2.gif" };
-LPWSTR gif_path[] = { MAKEINTRESOURCE(IDB_GIF1), MAKEINTRESOURCE(IDB_GIF2) };
+// const wchar_t* gif_path[] = { L"rsc\\c1.gif", L"rsc\\c2.gif" };
+LPWSTR gif_path[] = {MAKEINTRESOURCE(IDB_GIF1), MAKEINTRESOURCE(IDB_GIF2)};
 
 /*****************************************************/
 /*                                                   */
@@ -29,7 +29,7 @@ LPWSTR gif_path[] = { MAKEINTRESOURCE(IDB_GIF1), MAKEINTRESOURCE(IDB_GIF2) };
 /*****************************************************/
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-HWND creat_window_boarderless(HINSTANCE hInstance, const wchar_t* class_name, const wchar_t* window_name, int width, int height);
+HWND create_window_borderless(HINSTANCE hInstance, const wchar_t *class_name, const wchar_t *window_name, int width, int height);
 
 /*****************************************************/
 /*                                                   */
@@ -40,13 +40,13 @@ HWND creat_window_boarderless(HINSTANCE hInstance, const wchar_t* class_name, co
 class RandomLH // Random Number Generator: Lower and Higher
 {
 private:
-	std::random_device rd;
-	std::mt19937 gen;
-	std::uniform_int_distribution<int> dis;
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> dis;
 
 public:
-	RandomLH(int min, int max) : gen(rd()), dis(min, max) {}
-	int operator()() { return dis(gen); }
+    RandomLH(int min, int max) : gen(rd()), dis(min, max) {}
+    int operator()() { return dis(gen); }
 };
 
 /*****************************************************/
@@ -56,36 +56,36 @@ public:
 /*****************************************************/
 
 int WINAPI wWinMain(
-	_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR lpCmdLine,
-	_In_ int nShowCmd)
+    _In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR lpCmdLine,
+    _In_ int nShowCmd)
 {
-	try
-	{
-		HWND hwnd = creat_window_boarderless(hInstance, L"coin", L"coin", 480, 480);
+    try
+    {
+        HWND hwnd = create_window_borderless(hInstance, L"coin", L"coin", 480, 480);
 
-		// Set the transparency color, also works with the animation subwindow later
-		SetLayeredWindowAttributes(hwnd, BACKGROUND_COLOR, TRANSPARENCY, LWA_COLORKEY);
+        // Set the transparency color, also works with the animation subwindow later
+        SetLayeredWindowAttributes(hwnd, BACKGROUND_COLOR, TRANSPARENCY, LWA_COLORKEY);
 
-		ShowWindow(hwnd, nShowCmd);
-		UpdateWindow(hwnd);
+        ShowWindow(hwnd, nShowCmd);
+        UpdateWindow(hwnd);
 
-		MSG msg = {};
+        MSG msg = {};
 
-		while (GetMessageA(&msg, NULL, 0, 0))
-		{
-			TranslateMessage(&msg);
-			DispatchMessageW(&msg);
-		}
-	}
-	catch (const std::logic_error& e)
-	{
-		MessageBoxA(NULL, e.what(), "Error!", MB_ICONEXCLAMATION | MB_OK);
-		return 1;
-	}
+        while (GetMessageA(&msg, NULL, 0, 0))
+        {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
+    catch (const std::logic_error &e)
+    {
+        MessageBoxA(NULL, e.what(), "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
 
 /*****************************************************/
@@ -96,120 +96,120 @@ int WINAPI wWinMain(
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	// Get the instance of the GIF_PLAYER class from USERDATA
-	GIF_PLAYER* gif = reinterpret_cast<GIF_PLAYER*>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
-	switch (msg)
-	{
-	case WM_CREATE:
-	{
-		// Create a new instance of the GIF_PLAYER and play the GIF immediately
-		gif = new GIF_PLAYER(hwnd, gif_path[RandomLH(0, 1)()], BACKGROUND_COLOR);
-		gif->start();
-		SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(gif));
-	}
-	break;
-	case WM_CLOSE:
-		DestroyWindow(hwnd);
-		break;
-	case WM_DESTROY:
-	{
-		// Delete the GIF_PLAYER instance
-		if (gif != NULL)
-		{
-			delete gif;
-		}
-		PostQuitMessage(0);
-	}
-	break;
-	case WM_KEYDOWN:
-		switch (wParam)
-		{
-			// Press ESC to close the window
-		case VK_ESCAPE:
-			if (gif != NULL)
-			{
-				gif->cease();
-			}
-			DestroyWindow(hwnd);
-			break;
-			// Press SPACE to pause the GIF
-		case VK_SPACE:
-			if (gif != NULL)
-			{
-				gif->pause();
-			}
-			break;
-			// Press R to restart the GIF, the GIF will be randomly selected
-		case 'R':
-		case 'r':
-			if (gif != NULL)
-			{
-				gif->cease();
-				delete gif;
-				gif = NULL;
-			}
-			gif = new GIF_PLAYER(hwnd, gif_path[RandomLH(0, 1)()], BACKGROUND_COLOR);
-			gif->start();
-			SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(gif));
-			break;
-			// Press Q to go to the last frame of the GIF
-		case 'q':
-		case 'Q':
-			if (gif != NULL)
-			{
-				if (gif->gif_state != GIF_PLAYER::CEASE)
-				{
-					if (gif->gif_state == GIF_PLAYER::PLAY)
-						gif->pause(); // PLAY -> PAUSE
-					gif->current_frame = gif->get_frame_count() - 2;
-					gif->pause(); // PAUSE -> PLAY
-				}
-			}
-			break;
-		}
-		break;
-	default:
-		return DefWindowProc(hwnd, msg, wParam, lParam);
-	}
-	return 0;
+    // Get the instance of the GIF_PLAYER class from USERDATA
+    GIF_PLAYER *gif = reinterpret_cast<GIF_PLAYER *>(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
+    switch (msg)
+    {
+    case WM_CREATE:
+    {
+        // Create a new instance of the GIF_PLAYER and play the GIF immediately
+        gif = new GIF_PLAYER(hwnd, gif_path[RandomLH(0, 1)()], BACKGROUND_COLOR);
+        gif->start();
+        SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(gif));
+    }
+    break;
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    case WM_DESTROY:
+    {
+        // Delete the GIF_PLAYER instance
+        if (gif != NULL)
+        {
+            delete gif;
+        }
+        PostQuitMessage(0);
+    }
+    break;
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        // Press ESC to close the window
+        case VK_ESCAPE:
+            if (gif != NULL)
+            {
+                gif->cease();
+            }
+            DestroyWindow(hwnd);
+            break;
+        // Press SPACE to pause the GIF
+        case VK_SPACE:
+            if (gif != NULL)
+            {
+                gif->pause();
+            }
+            break;
+        // Press R to restart the GIF, the GIF will be randomly selected
+        case 'R':
+        case 'r':
+            if (gif != NULL)
+            {
+                gif->cease();
+                delete gif;
+                gif = NULL;
+            }
+            gif = new GIF_PLAYER(hwnd, gif_path[RandomLH(0, 1)()], BACKGROUND_COLOR);
+            gif->start();
+            SetWindowLongPtrW(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(gif));
+            break;
+        // Press Q to go to the last frame of the GIF
+        case 'q':
+        case 'Q':
+            if (gif != NULL)
+            {
+                if (gif->gif_state != GIF_PLAYER::CEASE)
+                {
+                    if (gif->gif_state == GIF_PLAYER::PLAY)
+                        gif->pause(); // PLAY -> PAUSE
+                    gif->current_frame = gif->get_frame_count() - 2;
+                    gif->pause(); // PAUSE -> PLAY
+                }
+            }
+            break;
+        }
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
 }
 
-HWND creat_window_boarderless(HINSTANCE hInstance, const wchar_t* class_name, const wchar_t* window_name, int width, int height)
+HWND create_window_borderless(HINSTANCE hInstance, const wchar_t *class_name, const wchar_t *window_name, int width, int height)
 {
-	WNDCLASSW wc = {
-		CS_HREDRAW | CS_VREDRAW,
-		WndProc,
-		0,
-		0,
-		hInstance,
-		LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)), // Load the icon from the rc file
-		LoadCursor(NULL, IDC_ARROW),
-		CreateSolidBrush(BACKGROUND_COLOR),
-		NULL,
-		class_name };
+    WNDCLASSW wc = {
+        CS_HREDRAW | CS_VREDRAW,
+        WndProc,
+        0,
+        0,
+        hInstance,
+        LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)), // Load the icon from the rc file
+        LoadCursor(NULL, IDC_ARROW),
+        CreateSolidBrush(BACKGROUND_COLOR),
+        NULL,
+        class_name};
 
-	if (!RegisterClassW(&wc))
-		throw std::logic_error("Window Registration Failed!");
+    if (!RegisterClassW(&wc))
+        throw std::logic_error("Window Registration Failed!");
 
-	// Calculate the position of the window, center of the screen
-	int posX = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
-	int posY = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
-	if (posX < 0 || posY < 0)
-		throw std::logic_error("Window Position Calculation Failed!");
+    // Calculate the position of the window, center of the screen
+    int posX = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
+    int posY = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
+    if (posX < 0 || posY < 0)
+        throw std::logic_error("Window Position Calculation Failed!");
 
-	HWND hwnd = CreateWindowExW(
-		WS_EX_LAYERED,
-		class_name,
-		window_name,
-		WS_POPUP,
-		posX, posY, width, height,
-		NULL,
-		NULL,
-		hInstance,
-		NULL);
+    HWND hwnd = CreateWindowExW(
+        WS_EX_LAYERED,
+        class_name,
+        window_name,
+        WS_POPUP,
+        posX, posY, width, height,
+        NULL,
+        NULL,
+        hInstance,
+        NULL);
 
-	if (hwnd == NULL)
-		throw std::logic_error("Window Creation Failed!");
+    if (hwnd == NULL)
+        throw std::logic_error("Window Creation Failed!");
 
-	return hwnd;
+    return hwnd;
 }
